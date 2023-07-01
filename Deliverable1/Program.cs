36180c4612dc3d078f91fa41abe762d8a69a63ab
Megan Tenerowicz <Megan Tenerowicz@LAPTOP-QVQSLJGF>
2023-07-01 10:07:38 -0400

    Add project files.

---
+++ Deliverable1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Enter an integer value between 1 and 100: ");
+            int userInput = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter 'Even' or 'Odd' to specify the series: ");
+            string seriesType = Console.ReadLine().ToLower();
+
+            Console.WriteLine($"You have selected the {seriesType} series. The numbers between 0 and {userInput} are:");
+
+            int i = 0;
+            while (i <= userInput)
+            {
+                if ((seriesType == "even" && i % 2 == 0) || (seriesType == "odd" && i % 2 != 0))
+                {
+                    Console.WriteLine(i);
+                }
+                i++;
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid input. Please enter a valid integer.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred.");
+        }
+    }
+}
